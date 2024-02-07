fa1216a476bee20aea01af23f3a098ce310de623
shivup5005 <shivup5005@gmail.com>
2024-02-07 18:03:08 +0000

    commit

--- src/pages/AddNewTask.jsx
+++ src/pages/AddNewTask.jsx
@@ -2,8 +2,15 @@ import { useParams } from "react-router-dom"
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 const Addnewtask = ()=>{
+    const [taskName,setTaskName] = useState()
+    const [disc,setDisc] = useState()
+    const [status,setStatus] = useState()
+
+    const taskDetails = {taskName,disc,status}
+    
     const {boardname} = useParams()
 
     const boardsDetails = useSelector(state=>{
@@ -12,34 +19,44 @@ const Addnewtask = ()=>{
     const currentBoardDetails = boardsDetails.filter(ele=>{
         return ele.boardName == boardname
     })
-    console.log(currentBoardDetails)
+    
     const options = currentBoardDetails[0].columns
 
       let defaultOption = options[0];
 
       const handleDropdown=(e)=>{
-        console.log(e.value)
+        
+        setStatus(e.value)
+      }
+
+      const handleCreate=(e)=>{
+        e.preventDefault()
+        console.log(taskDetails)
       }
    
     return (
         <div className="flex flex-col items-center justify-center gap-4 text-xl h-[95vh] text-[#161A30]">
-            <div className="bg-[#C7C8CC] px-10 py-20 flex flex-col items-center justify-center gap-4 rounded-md">
+            <form className="bg-[#C7C8CC] px-10 py-20 flex flex-col items-center justify-center gap-4 rounded-md">
                 <h1 className="text-2xl">Add new Task</h1>
             <div className="flex flex-col gap-2 w-full">
                 <label >Task Name</label>
-                <input type="text" className="border-2 border-black" required />
+                <input type="text" className="border-2 border-black" onChange={(e)=>{
+                    setTaskName(e.target.value)
+                }} required />
             </div>
             <div className="flex flex-col gap-2 w-full">
                 <label >Discription</label>
-                <textarea type="textarea" className="border-2 border-black w-full"  required/>
+                <textarea type="textarea" className="border-2 border-black w-full" onChange={(e)=>{
+                    setDisc(e.target.value)
+                }} required/>
             </div>
             <div className="flex flex-col gap-2 w-full">
                 <label >Current Status</label>
-                <Dropdown options={options}  onChange={handleDropdown} placeholder="Select an option" required='true'/>
+                <Dropdown options={options} onChange={handleDropdown}  placeholder="Select an option" required='true'/>
 
             </div>
-            <button className="bg-[#1D2B53]  py-1 w-full text-white">Create Task</button>
-            </div>
+            <input type="submit" className="bg-[#1D2B53]  py-1 w-full text-white" onSubmit={handleCreate} value='Create Task'/>
+            </form>
         </div>
 
     )
